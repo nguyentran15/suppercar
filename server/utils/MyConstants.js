82b1b50f1075dbc5450a4d0457eda4e32f4f0c28
Thinh2002 <duongphucthinh300702@gmail.com>
2023-07-14 22:44:38 +0700

    1

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'loser123.u1pzcor.mongodb.net',
+  DB_USER: 'canh177',
+  DB_PASS: 'canhga177',
+  DB_DATABASE: 'shoppingonline',
+  EMAIL_USER: 'nguyenduccanh177@gmail.com', // Microsoft mail service
+  EMAIL_PASS: 'rhllgmjsoazulbvh',
+  JWT_SECRET: '<jwt_secret>',   
+  JWT_EXPIRES: '60000000', // in milliseconds
+};
+module.exports = MyConstants;
